e991d547d21d620429ad2f17f29e0978f3073804
Vincent Jaquet <vincent.jaquet@edu.hefr.ch>
2020-11-04 09:40:54 +0100

    Initialize Angular 10 and disable AngularJS

--- karma.conf.js
+++ karma.conf.js
@@ -1,31 +1,46 @@
-var webpackConfig = require("./webpack.config");
-
-module.exports = function(config) {
+module.exports = function (config) {
 	config.set({
 		basePath: '.',
-		files: [
-			'test/**/*.js',
-		],
-		preprocessors: {
-			'js/**/*.js': ['webpack'],
-			'test/**/*.js': ['webpack'],
+		client: {
+			clearContext: false
 		},
+		coverageIstanbulReporter: {
+			dir: require('path').join(__dirname, './coverage/temporal-still'),
+			reports: [
+				'html',
+				'lcovonly',
+				'text-summary',
+			],
+			fixWebpackSourcePaths: true
+		},
+		reporters: [
+			'progress',
+			'kjhtml',
+		],
+		port: 9876,
+		colors: true,
+		logLevel: config.LOG_INFO,
+		singleRun: false,
+		restartOnFileChange: true,
 		autoWatch: true,
 		frameworks: [
 			'jasmine',
+			'@angular-devkit/build-angular',
 		],
 		browsers: [
 			'PhantomJS',
+			//'Chromium',
+			//'Chrome',
+			//'Firefox',
 		],
 		plugins: [
-			'karma-phantomjs-launcher',
-			'karma-jasmine',
-			'karma-webpack',
+			require('karma-jasmine'),
+			require('karma-phantomjs-launcher'),
+			require('karma-firefox-launcher'),
+			require('karma-chrome-launcher'),
+			require('karma-jasmine-html-reporter'),
+			require('karma-coverage-istanbul-reporter'),
+			require('@angular-devkit/build-angular/plugins/karma')
 		],
-		webpack: {
-			mode: 'production',
-			//devtool: 'inline-source-map',
-			module: webpackConfig.module
-		},
 	});
 };
