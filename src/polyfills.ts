9525b3be69b35e4991809641634d161cdf57fb83
Vincent Jaquet <vincent.jaquet@edu.hefr.ch>
2020-11-11 09:01:56 +0100

    Replace custom polyfills by mdn-polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -3,9 +3,7 @@
 
 import 'classlist.js';
 
-import Util from './engine/helpers/Util';
-Util.ensureForEach();
-Util.ensureEvery();
+import 'mdn-polyfills/Array.prototype.forEach';
 
 // Zone JS is required by default for Angular itself.
 import 'zone.js/dist/zone';  // Included with Angular CLI.
